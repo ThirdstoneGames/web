cf490f57475263f378227fd9b99e34a2351d32b7
ThirdStomeGames <thirdstonegames@gmail.com>
2023-06-29 02:32:30 +0900

    server.on('connection', ..) 에서 응답을 처리

--- httpweb.js
+++ httpweb.js
@@ -1,9 +1,20 @@
 var http = require('http');
 var server = http.createServer();
 var port = 3000;
-server.listen(port, function()
+server.listen(port, function(req, res)
 {
     console.log('Web Server Started...');
+    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+    res.write("<!DOCTYPE html");
+    res.write("<html>");
+    res.write(" <head>");
+    res.write("     <title>응답 페이지</title>");
+    res.write(" </head>");
+    res.write(" <body>");
+    res.write("     <h1>Response page from Node.js</h1>");
+    res.write(" </body>");
+    res.write("</html>");
+    res.end();
 })
 
 server.on('connection', () => 
@@ -11,6 +22,7 @@ server.on('connection', () =>
     console.log('someone connected!');
 })
 
+/*
 server.on('request', function(req, res)
 {
     res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
@@ -25,4 +37,5 @@ server.on('request', function(req, res)
     res.write("</html>");
     res.end();
 })
+*/
 
