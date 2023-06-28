3d51daf41fab1f752529ff822b972864cec20737
ThirdStomeGames <thirdstonegames@gmail.com>
2023-06-29 02:29:47 +0900

    node.js 의 http 를 이용한 서버 기본. server.on 을 이용한 응답요청

---
+++ httpweb.js
@@ -0,0 +1,28 @@
+var http = require('http');
+var server = http.createServer();
+var port = 3000;
+server.listen(port, function()
+{
+    console.log('Web Server Started...');
+})
+
+server.on('connection', () => 
+{
+    console.log('someone connected!');
+})
+
+server.on('request', function(req, res)
+{
+    res.writeHead(200, {"Content-Type":"text/html; charset=utf-8"});
+    res.write("<!DOCTYPE html");
+    res.write("<html>");
+    res.write(" <head>");
+    res.write("     <title>응답 페이지</title>");
+    res.write(" </head>");
+    res.write(" <body>");
+    res.write("     <h1>Response page from Node.js</h1>");
+    res.write(" </body>");
+    res.write("</html>");
+    res.end();
+})
+
