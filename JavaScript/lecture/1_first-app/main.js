f8bec7b3b85f4596d280b599ade0ba167ad8891b
ThirdStomeGames <thirdstonegames@gmail.com>
2023-10-30 22:28:21 +0900

    JS변수, 오브젝트타입, 함수, 이벤트, DOM 기초

--- JavaScript/lecture/1_first-app/main.js
+++ JavaScript/lecture/1_first-app/main.js
@@ -1,5 +1,40 @@
-console.log("Hello World");
+// console.log
+// 변수
 
+console.log("Hello World");
+console.log('Hello@@@');
+console.log('hello@@@','world','javascript');
+
+let str='Hello Javascript';
+let number=30;
+console.log("문자열" + str);
+console.log("숫자" + number);
+console.log("문자열" ,str);
+console.log("숫자" , number);
+
+
+// function
+function sayHello()
+{
+    alert("Say!! Hello");
+    console.log("normal result");
+}
+
+
+// object type
+let obj = {
+    number:30, // property
+    sayHello: function() {
+        console.log('obj->Hello1');
+        console.log('obj->Hello2');
+        console.log('obj->Hello3');
+    },
+};
+
+console.log("object type", obj);
+
+
+// DOM
 // 1. "운동" 을 "헬스3대운동" 으로 변경
 // 2. 클래스 네임이 item 인 요소를 모두출력
 // 3. HTML 태그가 "li" 인 것을 모두 출력
